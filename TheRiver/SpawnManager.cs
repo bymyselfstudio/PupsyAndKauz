1f245630d30f623c0f1de7446269102dd5c47a37
guitarpeggio <94455238+guitarpeggio@users.noreply.github.com>
2021-11-16 13:45:48 +0100

    Create SpawnManager.cs

---
+++ TheRiver/SpawnManager.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    // implement DespawnArea script --> first attempt didn't work
+    // events will just get triggered if assigned to trigger object --> either spawnItems or despawner
+    
+    // implement levelKey spawn when level score is reached
+    // implement method which checks if heartPrefab is active, so no other heart should spawn
+
+
+    [SerializeField] GameObject[] obstablePrefabs;
+    private readonly float xRange = 8.0f;
+    private readonly float zRangeMax = 80.0f;
+    private readonly float zRangeMin = 60.0f;
+
+    void Start()
+    {
+        InvokeRepeating("RandomSpawnItem", 1, 1.5f);
+    }
+
+    private void RandomSpawnItem()
+    {
+        int itemIndex = Random.Range(0, obstablePrefabs.Length);
+        Instantiate(obstablePrefabs[itemIndex], RandomSpawnPosition(), obstablePrefabs[itemIndex].transform.rotation);
+    }
+    
+    private Vector3 RandomSpawnPosition()
+    {
+        // several models don't match the local unity orientation, so spawnpos is not set correctly!
+        float ySpawnPos = ((float)Space.Self); // only works with correct local orientation!
+        float xSpawnPos = Random.Range(-xRange, xRange);
+        float zSpawnPos = Random.Range(zRangeMin, zRangeMax);
+        Vector3 spawnPos = new Vector3(xSpawnPos, ySpawnPos, zSpawnPos);
+        return spawnPos;
+    }
+
+    // IMPLEMENT LATER WHEN HEARTS CAN BE COLLECTED
+    //IEnumerator BulletproofCooldown()
+    //{
+    //    yield return new WaitForSeconds(5);
+    //}
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    StartCoroutine(BulletproofCooldown());
+    //}
+}
